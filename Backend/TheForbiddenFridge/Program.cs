50c683db106fb4d0c8b36cce0c1cb20ad6afb728
Veggissss <vegarddem@gmail.com>
2026-01-28 15:05:00 +0100

    Setup JWT and role configurations.

--- Backend/TheForbiddenFridge/Program.cs
+++ Backend/TheForbiddenFridge/Program.cs
@@ -1,4 +1,8 @@
+using System.Text;
 using dotenv.net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
 using TheForbiddenFridge.DbContexts;
 using TheForbiddenFridge.Models;
 using TheForbiddenFridge.Repositories;
@@ -6,6 +10,37 @@ using TheForbiddenFridge.Repositories;
 DotEnv.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtKey = builder.Configuration["JwtKey"] ?? throw new ArgumentNullException("JwtKey");
+var jwtIssuer = builder.Configuration["JwtIssuer"] ?? throw new ArgumentNullException("JwtIssuer");
+var jwtAudience = builder.Configuration["JwtAudience"] ?? throw new ArgumentNullException("JwtAudience");
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtKey)),
+    };
+});
+
+
+builder.Services.AddAuthorizationBuilder()
+    .AddDefaultPolicy("User",
+        new AuthorizationPolicyBuilder().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+            .RequireAuthenticatedUser().Build())
+    .AddPolicy("Admin",
+        new AuthorizationPolicyBuilder().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+            .RequireAuthenticatedUser().Build())
+    .AddPolicy("StoreAdmin",
+        new AuthorizationPolicyBuilder().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+            .RequireAuthenticatedUser().Build());
 
 // Add services to the container.
 
@@ -22,6 +57,9 @@ builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -35,5 +73,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
