2970dc7c716a21cd25b468e5bd176c757668e06e
MWillu <martinbrede98@gmail.com>
2026-01-29 09:32:23 +0100

    Made AuthController.cs

---
+++ Backend/TheForbiddenFridge/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheForbiddenFridge.DTOs;
+using TheForbiddenFridge.Models;
+using TheForbiddenFridge.Repositories;
+using TheForbiddenFridge.Service;
+
+namespace TheForbiddenFridge.Controllers;
+
+[ApiController]
+public class AuthController(IUserRepository userRepository, JwtIssuerService jwtService) : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] LoginDTO register)
+    {
+        if (userRepository.GetAll().Any(user => user.Username == register.Username))
+        {
+            return Unauthorized("Username already exists");
+        }
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(register.Password);
+        
+        userRepository.Create(new User(register.Username, hashedPassword));
+        return Ok("created user with username: " + register.Username);
+    }
+    
+    
+    [HttpPost]
+    public IActionResult Login([FromBody] LoginDTO login)
+    {
+        var user = userRepository.GetAll().FirstOrDefault(user => user.Username == login.Username);
+        if (user == null)
+        {
+            return NotFound("User not found");
+        }
+        bool verified = BCrypt.Net.BCrypt.Verify(login.Password, user.Password);
+        if (!verified)
+        {
+            return Unauthorized("Invalid password");
+        }
+
+        var token = jwtService.CreateJwt(user.Id.ToString(), user.Username, new List<string>() { "user" });
+        return Ok(token); 
+
+
+    }
+}
