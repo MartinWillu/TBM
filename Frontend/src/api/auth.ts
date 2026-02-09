eab9814500ab1e0068d7a39014266f1acdfcde9e
Veggissss <vegarddem@gmail.com>
2026-02-09 15:20:58 +0100

    feat: implement registration page using form component with error logs

---
+++ Frontend/src/api/auth.ts
@@ -0,0 +1,12 @@
+import type { UserInfo } from "../types";
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export async function registerUser(userInfo: UserInfo) {
+    //const response = await fetch("backend/api/auth/register")
+    throw new Error("Not implemented yet");
+}
+
+export async function loginUser(userInfo: UserInfo) {
+    throw new Error("Not implemented yet");
+}
