1e424c68f26b39b7bb2bc70daa06b1f038fd9c10
Veggissss <vegarddem@gmail.com>
2026-02-10 14:06:52 +0100

    feat: add mock data.

--- Frontend/src/types.ts
+++ Frontend/src/types.ts
@@ -3,3 +3,31 @@ export interface UserInfo {
     username: string,
     password: string,
 }
+
+export interface Category {
+    id: number;
+    name: string;
+}
+
+export interface Store {
+    id: number;
+    name: string;
+    logoUrl: string;
+    userId: number;
+}
+
+export interface Grocery {
+    id: number;
+    name: string;
+    currentPrice: number;
+    oldPrice: number;
+    quantity: number;
+    storeId: number;
+    categoryId?: number;
+    categories?: Category[];
+}
+
+export interface UserUpdate {
+    username?: string;
+    [key: string]: string | undefined;
+}
