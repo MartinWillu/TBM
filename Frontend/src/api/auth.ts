db2ef5ebec75043f9daa0637c441372728a4e4fe
Veggissss <vegarddem@gmail.com>
2026-02-10 07:38:25 +0100

    feat: refactor TODO auth functions syntax, added checkAuthorized.

--- Frontend/src/api/auth.ts
+++ Frontend/src/api/auth.ts
@@ -2,11 +2,15 @@ import type { UserInfo } from "../types";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-export async function registerUser(userInfo: UserInfo) {
+export const registerUser = async (userInfo: UserInfo) => {
     //const response = await fetch("backend/api/auth/register")
     throw new Error("Not implemented yet");
 }
 
-export async function loginUser(userInfo: UserInfo) {
+export const loginUser = async (userInfo: UserInfo) => {
+    throw new Error("Not implemented yet");
+}
+
+export const checkAuthorized = async () => {
     throw new Error("Not implemented yet");
 }
