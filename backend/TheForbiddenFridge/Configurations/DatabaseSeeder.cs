c8b55e4ab88f1c6583afa5140b45822d732acafd
MWillu <martinbrede98@gmail.com>
2026-02-26 13:26:49 +0100

    reseeded tables

--- backend/TheForbiddenFridge/Configurations/DatabaseSeeder.cs
+++ backend/TheForbiddenFridge/Configurations/DatabaseSeeder.cs
@@ -89,23 +89,23 @@ public class DatabaseSeeder
         var stores = new List<Store>
         {
             new() {
-                Name = "Fresh Market",
-                LogoUrl = "https://placehold.co/200x200/4CAF50/ffffff?text=Fresh+Market",
+                Name = "Rema 1000",
+                LogoUrl = "https://www.pngkit.com/png/detail/358-3582531_rema-1000-logo.png",
                 UserId = users[2].Id
             },
             new() {
-                Name = "Green Grocers",
-                LogoUrl = "https://placehold.co/200x200/8BC34A/ffffff?text=Green+Grocers",
+                Name = "KIWI",
+                LogoUrl = "https://www.eliteprosjekt.no/wp-content/uploads/2021/05/bunnpris-logo-digital-768x145.jpg",
                 UserId = users[2].Id
             },
             new() {
-                Name = "Super Saver",
-                LogoUrl = "https://placehold.co/200x200/FF5722/ffffff?text=Super+Saver",
+                Name = "Bunnpris",
+                LogoUrl = "https://www.eliteprosjekt.no/wp-content/uploads/2021/05/bunnpris-logo-digital-768x145.jpg",
                 UserId = users[3].Id
             },
             new() {
-                Name = "Organic Corner",
-                LogoUrl = "https://placehold.co/200x200/009688/ffffff?text=Organic+Corner",
+                Name = "Coop Extra",
+                LogoUrl = "https://tse3.mm.bing.net/th/id/OIP.bkP2KRhXtpo176lRB5Js4AHaEj?rs=1&pid=ImgDetMain&o=7&rm=3",
                 UserId = users[3].Id
             }
         };
@@ -119,7 +119,7 @@ public class DatabaseSeeder
                 CurrentPrice = 3.99f,
                 OldPrice = 4.99f,
                 Quantity = 100,
-                ImageUrl = "https://placehold.co/600x400/FF5252/ffffff?text=Organic+Apples",
+                ImageUrl = "https://tse2.mm.bing.net/th/id/OIP.AmcGlWm6tLVFs54C1qGNVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3",
                 StoreId = stores[0].Id,
                 CategoryId = categories[0].Id
             },
@@ -128,7 +128,7 @@ public class DatabaseSeeder
                 CurrentPrice = 1.99f,
                 OldPrice = 2.49f,
                 Quantity = 150,
-                ImageUrl = "https://placehold.co/600x400/FFEB3B/000000?text=Bananas",
+                ImageUrl = "https://static.fanpage.it/wp-content/uploads/sites/22/2018/06/istock-162487071.jpg",
                 StoreId = stores[0].Id,
                 CategoryId = categories[0].Id
             },
@@ -137,7 +137,7 @@ public class DatabaseSeeder
                 CurrentPrice = 3.49f,
                 OldPrice = 3.99f,
                 Quantity = 50,
-                ImageUrl = "https://placehold.co/600x400/E3F2FD/000000?text=Whole+Milk",
+                ImageUrl = "https://tse4.mm.bing.net/th/id/OIP.Yrabrg2WnCHzaEVSHWIs-gHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
                 StoreId = stores[0].Id,
                 CategoryId = categories[2].Id
             },
@@ -146,7 +146,7 @@ public class DatabaseSeeder
                 CurrentPrice = 5.99f,
                 OldPrice = 6.99f,
                 Quantity = 30,
-                ImageUrl = "https://placehold.co/600x400/FFC107/000000?text=Cheddar+Cheese",
+                ImageUrl = "https://bilder.ngdata.no/5000295080836/meny/large.jpg",
                 StoreId = stores[0].Id,
                 CategoryId = categories[2].Id
             },
@@ -155,7 +155,7 @@ public class DatabaseSeeder
                 CurrentPrice = 2.49f,
                 OldPrice = 2.99f,
                 Quantity = 80,
-                ImageUrl = "https://placehold.co/600x400/FF9800/ffffff?text=Carrots",
+                ImageUrl = "https://tse1.mm.bing.net/th/id/OIP.NQoblU6aVDk4w-pjm_mqRgHaGD?rs=1&pid=ImgDetMain&o=7&rm=3",
                 StoreId = stores[1].Id,
                 CategoryId = categories[1].Id
             },
@@ -164,7 +164,7 @@ public class DatabaseSeeder
                 CurrentPrice = 2.99f,
                 OldPrice = 3.49f,
                 Quantity = 60,
-                ImageUrl = "https://placehold.co/600x400/4CAF50/ffffff?text=Broccoli",
+                ImageUrl = "https://bilder.ngdata.no/4349/meny/large.jpg",
                 StoreId = stores[1].Id,
                 CategoryId = categories[1].Id
             },
@@ -173,7 +173,7 @@ public class DatabaseSeeder
                 CurrentPrice = 4.99f,
                 OldPrice = 5.99f,
                 Quantity = 40,
-                ImageUrl = "https://placehold.co/600x400/E91E63/ffffff?text=Strawberries",
+                ImageUrl = "https://tse1.mm.bing.net/th/id/OIP.KD5TiA6k_Ug8SWjYmxQ4ZgHaIr?rs=1&pid=ImgDetMain&o=7&rm=3",
                 StoreId = stores[1].Id,
                 CategoryId = categories[0].Id
             },
@@ -182,7 +182,7 @@ public class DatabaseSeeder
                 CurrentPrice = 7.99f,
                 OldPrice = 9.99f,
                 Quantity = 25,
-                ImageUrl = "https://placehold.co/600x400/FFCCBC/000000?text=Chicken+Breast",
+                ImageUrl = "https://images.eatthismuch.com/site_media/img/451_erin_m_d7cfcfcd-642a-4d6b-b6e8-0adf3eabbff7.png",
                 StoreId = stores[2].Id,
                 CategoryId = categories[3].Id
             },
@@ -191,7 +191,7 @@ public class DatabaseSeeder
                 CurrentPrice = 5.99f,
                 OldPrice = 7.49f,
                 Quantity = 35,
-                ImageUrl = "https://placehold.co/600x400/D7CCC8/000000?text=Ground+Beef",
+                ImageUrl = "https://tse2.mm.bing.net/th/id/OIP.SbO9dFhscf7bSdo8kPe_0gHaFf?rs=1&pid=ImgDetMain&o=7&rm=3",
                 StoreId = stores[2].Id,
                 CategoryId = categories[3].Id
             },
@@ -200,7 +200,7 @@ public class DatabaseSeeder
                 CurrentPrice = 4.49f,
                 OldPrice = 5.49f,
                 Quantity = 45,
-                ImageUrl = "https://placehold.co/600x400/FF9800/ffffff?text=Orange+Juice",
+                ImageUrl = "https://tse4.mm.bing.net/th/id/OIP.HRGD_yir6cfz5uXgldOizQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
                 StoreId = stores[2].Id,
                 CategoryId = categories[4].Id
             },
@@ -209,7 +209,7 @@ public class DatabaseSeeder
                 CurrentPrice = 3.99f,
                 OldPrice = 4.49f,
                 Quantity = 55,
-                ImageUrl = "https://placehold.co/600x400/8D6E63/ffffff?text=Whole+Wheat+Bread",
+                ImageUrl = "https://bilder.ngdata.no/9108/meny/large.jpg",
                 StoreId = stores[3].Id,
                 CategoryId = categories[5].Id
             },
@@ -218,7 +218,7 @@ public class DatabaseSeeder
                 CurrentPrice = 5.99f,
                 OldPrice = 6.99f,
                 Quantity = 20,
-                ImageUrl = "https://placehold.co/600x400/FFECB3/000000?text=Croissants",
+                ImageUrl = "https://th.bing.com/th/id/R.273af4f1b013443c7730003b8a980a95?rik=qDCmug3OR0%2fhcw&riu=http%3a%2f%2fepicureandculture.com%2fwp-content%2fuploads%2f2014%2f12%2fshutterstock_172040546.jpg&ehk=INlZHKFggsSlVC7GWhz9LSsU3xMP1uXGKb%2fhu76DsWg%3d&risl=&pid=ImgRaw&r=0",
                 StoreId = stores[3].Id,
                 CategoryId = categories[5].Id
             },
@@ -227,7 +227,7 @@ public class DatabaseSeeder
                 CurrentPrice = 4.49f,
                 OldPrice = 5.49f,
                 Quantity = 100,
-                ImageUrl = "https://placehold.co/600x400/795548/ffffff?text=Granola+Bars",
+                ImageUrl = "https://i.ebayimg.com/images/g/fcIAAeSwUUZpgKPJ/s-l1600.webp",
                 StoreId = stores[3].Id,
                 CategoryId = categories[6].Id
             },
@@ -236,7 +236,7 @@ public class DatabaseSeeder
                 CurrentPrice = 6.99f,
                 OldPrice = 8.99f,
                 Quantity = 30,
-                ImageUrl = "https://placehold.co/600x400/BF360C/ffffff?text=Frozen+Pizza",
+                ImageUrl = "https://bilder.ngdata.no/4001724039112/meny/large.jpg",
                 StoreId = stores[2].Id,
                 CategoryId = categories[7].Id
             }
